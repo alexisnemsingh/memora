29796684ebee8710322fb9c7a5415e9d681e1b62
alexisnemsingh <alexisnemsingh@gmail.com>
2024-12-03 18:15:24 -0800

    project starter files

---
+++ Memora/MemoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoraApp.swift
+//  Memora
+//
+//  Created by Alexis Nemsingh on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoraTests/MemoraTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoraTests.swift
+//  MemoraTests
+//
+//  Created by Alexis Nemsingh on 12/3/24.
+//
+
+import Testing
+@testable import Memora
+
+struct MemoraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
